e0525d6724bf304ae1f7b5b7ef5abfaf1bbd7908
Peyton Thibodeaux <peytonthibodeaux@Peytons-MacBook-Pro-2.local>
2022-08-07 16:38:21 -0500

    add restaurunatee list

--- Dinder/lib/globals.dart
+++ Dinder/lib/globals.dart
@@ -2,3 +2,5 @@ enum Swipe { left, right, none }
 
 
 String SERVER_ENDPOINT = "dolphin-app-ykfke.ondigitalocean.app";
+
+List<String> restaurants = ["Mcdonalds", "Chick-fil-a", "Taco Bell", "Olive Garden", "Chilis", "Braums", "Mooyah", "Canes", "5 guys", "in-n-out", "chiptole", "Crooked Crust"];
