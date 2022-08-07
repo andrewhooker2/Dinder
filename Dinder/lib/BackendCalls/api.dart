d3ca7242e5110364c9cbac922e2eaa14de2ccb64
Peyton Thibodeaux <peytonthibodeaux@Peytons-MacBook-Pro-2.local>
2022-08-07 16:58:47 -0500

    backend has come

--- Dinder/lib/BackendCalls/api.dart
+++ Dinder/lib/BackendCalls/api.dart
@@ -1,6 +1,7 @@
 import 'package:http/http.dart' as http;
 
 import '../globals.dart' as globals;
+import '../Model/response.dart' as model;
 
 import 'dart:convert';
 import 'dart:io';
@@ -8,8 +9,7 @@ import 'dart:io';
 // tests
 void main() async{
   //print("resp: " + await Alive());
-  print(await CreateRoom("1234"));
-
+  //print(await CreateRoom("1234"));
 }
 
 // Create a room, put the user in the room, get a list of restaurants back
@@ -27,7 +27,7 @@ Future<String> Alive() async {
 }
 
 // Create a room, put the user in the room, get a list of restaurants back
-Future<Object> CreateRoom(String userID) async {
+Future<List<String>> CreateRoom(String userID) async {
   var queryParams = {
     'user': userID,
   };
@@ -37,9 +37,50 @@ Future<Object> CreateRoom(String userID) async {
     headers: {"Accept": "application/json"},
   );
   if (response.statusCode == 200) {
-    print(json.decode(response.body));
-    return response.body;
+    return globals.Restaurants;
   } else {
     return <String>[];
   }
+}
+
+// Remove a user from a room
+Future<void> LeaveRoom(String userID, String roomID) async {
+  var queryParams = {
+    'user': userID,
+    'room': roomID,
+  };
+  var uri = Uri.http(globals.SERVER_ENDPOINT, '/leave', queryParams);
+  http.get(
+    uri,
+    headers: {"Accept": "application/json"},
+  );
+}
+
+// Have a user vote for a restaurant
+Future<model.Response> SwipeRight(String restaurant, String roomID) async {
+  var queryParams = {
+    'restaurant': restaurant,
+    'room': roomID,
+  };
+  var uri = Uri.http(globals.SERVER_ENDPOINT, '/swipeRight', queryParams);
+  final response = await http.get(
+    uri,
+    headers: {"Accept": "application/json"},
+  );
+
+  return model.Response.fromJson(jsonDecode(response.body));
+}
+
+// Have a user vote for a restaurant
+Future<model.Response> SwipeLeft(String roomID) async {
+  var queryParams = {
+    'room': roomID,
+  };
+  var uri = Uri.http(globals.SERVER_ENDPOINT, '/swipeLeft', queryParams);
+  final response = await http.get(
+    uri,
+    headers: {"Accept": "application/json"},
+  );
+
+  return model.Response.fromJson(jsonDecode(response.body));
 }
