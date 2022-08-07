dc2a53eec7a7b0002c192429a40e162e3e7e31ff
Andrew Hooker <andrewhooker2@gmail.com>
2022-08-07 00:28:23 -0500

    Touching up CodeEntry and adding Configuration Page / route

--- Dinder/lib/Screens/CodeEntryPage.dart
+++ Dinder/lib/Screens/CodeEntryPage.dart
@@ -1,10 +1,14 @@
+
+
 import 'package:dinder/Common/NumberInputWidget.dart';
 import 'package:dinder/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import '../Common/NavigateButton.dart';
 
 class CodeEntryPage extends StatefulWidget {
   const CodeEntryPage({Key? key}) : super(key: key);
+
 
   @override
   _CodeEntryPageState createState() => _CodeEntryPageState();
@@ -13,6 +17,9 @@ class CodeEntryPage extends StatefulWidget {
 class _CodeEntryPageState extends State<CodeEntryPage> {
   @override
   Widget build(BuildContext context) {
+    var pageWidth = MediaQuery.of(context).size.width / 1.4;
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Dinder"),
@@ -28,25 +35,35 @@ class _CodeEntryPageState extends State<CodeEntryPage> {
             setState(() {});
           },
           child: Scaffold(
-           body: Column(
-             children: [
-               Text(
-                   "Enter in Room Code",
-                 style: TextStyle(
-                   fontWeight: FontWeight.bold,
-                 ),
+           body: Padding(
+             padding: EdgeInsets.symmetric(vertical: 0, horizontal: 60),
+             child: Container(
+
+               child: Column(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 crossAxisAlignment: CrossAxisAlignment.center,
+                 children: [
+                   const Text(
+                       "Wanting to Vote?",
+                     style: TextStyle(
+                       fontWeight: FontWeight.bold,
+                       fontSize: 40
+                     ),
+                   ),
+                   TextField(
+                     decoration: const InputDecoration(
+                         border: UnderlineInputBorder(),
+                         labelText: "Enter in Room Code"
+                     ),
+                     keyboardType: TextInputType.phone,
+                     inputFormatters: <TextInputFormatter>[
+                       FilteringTextInputFormatter.digitsOnly
+                     ], // Only numbers can be entered
+                   ),
+                   NavigateButton(context),
+                 ],
                ),
-               TextField(
-                 decoration: InputDecoration(
-                     border: UnderlineInputBorder(),
-                     labelText: "Enter in your code"
-                 ),
-                 keyboardType: TextInputType.phone,
-                 inputFormatters: <TextInputFormatter>[
-                   FilteringTextInputFormatter.digitsOnly
-                 ], // Only numbers can be entered
-               ),
-             ],
+             ),
            ),
           ),
         ),
@@ -55,6 +72,33 @@ class _CodeEntryPageState extends State<CodeEntryPage> {
   }
 }
 
+
+
+Widget NextButton(
+    BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 15),
+    child: Container(
+      width: MediaQuery.of(context).size.width / 1.4,
+      height: MediaQuery.of(context).size.height / 11,
+      decoration: BoxDecoration(
+        color: AppTheme.kColor1,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, "/VotingPage");
+          },
+        child: Text("Join Room"),
+      ),
+
+      // child: Padding(
+      //   padding: const EdgeInsets.all(12.0),
+      //   child: NavigateButton(asset: image, inputText: text, route: route),
+      // ),
+    ),
+  );
+}
 
 /*
 TextField(
---
+++ Dinder/lib/Screens/ConfigurationPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class ConfigurationPage extends StatefulWidget {
+  const ConfigurationPage({Key? key}) : super(key: key);
+
+  @override
+  State<ConfigurationPage> createState() => _ConfigurationPageState();
+}
+
+class _ConfigurationPageState extends State<ConfigurationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("hey this is the configuration page"),
+    );
+  }
+}
