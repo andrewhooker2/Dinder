1f1bee707bb5d683aa7cfba556a69366cd58db13
Andrew Hooker <andrewhooker2@gmail.com>
2022-08-07 18:14:34 -0500

    Adding results page

--- Dinder/lib/Screens/ResultsPage.dart
+++ Dinder/lib/Screens/ResultsPage.dart
@@ -1,4 +1,10 @@
+import 'package:dinder/Common/NavButton.dart';
+import 'package:dinder/constants.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:dinder/globals.dart' as globals;
+
+import '../Common/background_wave.dart';
 
 class ResultsPage extends StatefulWidget {
   const ResultsPage({Key? key}) : super(key: key);
@@ -10,8 +16,35 @@ class ResultsPage extends StatefulWidget {
 class _ResultsPageState extends State<ResultsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("I am the results page"),
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: Column(
+          children: [
+            BackgroundWave(
+              height: 180,
+              text: "Dinder",
+            ),
+            //Text("We have a winner!!!"),
+            Container(
+              child: Image(
+                fit: BoxFit.contain,
+                width: 250,
+                height: 250,
+                image: NetworkImage('https://exchangegwinnett.com/wp-content/uploads/2020/03/ChipotleMexicanGrills.jpeg'),
+                ),
+              ),
+
+            Padding(
+              padding: const EdgeInsets.all(80),
+              child: Container(
+                color: Colors.amberAccent,
+                child: NavButton(inputText: "Finally! Go Eat!", navRoute: "/HomeScreen"),
+
+    ),
+            )
+          ]
+      ),
+
     );
   }
 }
