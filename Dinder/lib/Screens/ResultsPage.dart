0ce044cb865fa720253bd5cfb64422d1de11985a
Peyton Thibodeaux <peytonthibodeaux@Peytons-MacBook-Pro-2.local>
2022-08-07 18:23:00 -0500

    added coconut.jpg back in

--- Dinder/lib/Screens/ResultsPage.dart
+++ Dinder/lib/Screens/ResultsPage.dart
@@ -38,7 +38,7 @@ class _ResultsPageState extends State<ResultsPage> {
               padding: const EdgeInsets.all(80),
               child: Container(
                 color: Colors.amberAccent,
-                child: NavButton(inputText: "Finally! Go Eat!", navRoute: "/HomeScreen"),
+                child: NavButton(inputText: "Finally! Go Eat!", navRoute: "/HomeScreen", backendCall: "coconut.jpg",),
 
     ),
             )
